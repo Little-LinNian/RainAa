dc3908df8d1ecd0255e6463ff2a7c032929683de
Little-LinNian <2544704967@qq.com>
2023-01-17 23:39:43 +0800

    🐛 Bug Fix

--- module/aichat.py
+++ module/aichat.py
@@ -1,24 +1,41 @@
 import random
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.app import Ariadne
-from graiax.shortcut.saya import listen,dispatch,decorate
-from graia.ariadne.entry import Group, Member,Source,DetectPrefix
+from graiax.shortcut.saya import listen, dispatch, decorate
+from graia.ariadne.entry import Group, Member, Source, DetectPrefix, At, Plain
 from graia.ariadne.util.cooldown import CoolDown
 from rainaa.perm import Ban, PermissionDispatcher
 import kayaku
 from rainaa.config import GPT2Config
 import aiohttp
 
+
 @listen("GroupMessage")
 @dispatch(CoolDown(0.7))
 @dispatch(PermissionDispatcher(Ban("aichat", ["本群还没开放"])))
-async def aichat(app: Ariadne, message: MessageChain, group: Group, member: Member,source: Source):
+async def aichat(
+    app: Ariadne, message: MessageChain, group: Group, member: Member, source: Source
+):
     cfg = kayaku.create(GPT2Config)
     url = cfg.url
     if url == "":
         await app.send_group_message(group, "机器人还没配置好呢")
         return
-    text =  message.display
+    if message.has(At) and message.get(At)[0].target == app.account:
+        text = message.include(Plain).display
+        async with aiohttp.ClientSession() as s:
+            if cfg.global_mode:
+                session = 0
+            session = group.id
+            async with s.get(f"{url}/?session={session}&text={text}") as resp:
+                if not resp.status == 200:
+                    return
+                data = await resp.json()
+                await app.send_group_message(group, data["text"],quote=source)
+    if str(group.id) not in cfg.group_probability:
+        cfg.group_probability[str(group.id)] = 0
+        kayaku.save_all()
+    text = message.display
     if text == "":
         return
     if str(group.id) not in cfg.group_probability:
@@ -43,9 +60,11 @@ async def aichat(app: Ariadne, message: MessageChain, group: Group, member: Memb
 @dispatch(CoolDown(0.7))
 @dispatch(PermissionDispatcher(Ban("aichat", ["本群还没开放"])))
 @decorate(DetectPrefix("设置ai概率"))
-async def set_probability(app: Ariadne, message: MessageChain, group: Group, member: Member,source: Source):
+async def set_probability(
+    app: Ariadne, message: MessageChain, group: Group, member: Member, source: Source
+):
     cfg = kayaku.create(GPT2Config)
-    text =  message.display.removeprefix("设置ai概率")
+    text = message.display.removeprefix("设置ai概率")
     if text == "":
         return
     try:
